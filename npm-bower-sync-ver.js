865e706ff93a7f9bee9cc6aeabeabacd388c4bdc
Luís Nabais <lnabais@impresa.pt>
2017-09-20 11:41:00 +0100

    Add a way to specify which files to target

--- npm-bower-sync-ver.js
+++ npm-bower-sync-ver.js
@@ -3,24 +3,41 @@
 const fs = require('fs')
 const program = require('commander')
 const meta = require(`${process.cwd()}/package.json`)
+const path = require('path')
+
+const DEFAULT_FILE_PATHS = [ './bower.json' ]
+
+function collect (val, memo) {
+  memo.push(val)
+  return memo
+}
+
+function updateVersion (file, ver) {
+  const resolved = path.resolve(file)
+
+  let fileStr = fs.readFileSync(resolved, 'utf-8')
+  const verPtn = /(.*"version"\s*:\s*")[^"]*(".*)/
+
+  if (verPtn.test(fileStr)) {
+    fileStr = fileStr.replace(verPtn, '$1' + ver + '$2')
+    fs.writeFileSync(resolved, fileStr, 'utf-8')
+  }
+}
 
 try {
   let ver = meta.version
 
   program
     .version(meta.version)
+    .option('-f, --file [file]', 'The target file to sync. Defaults to bower.json', collect, DEFAULT_FILE_PATHS)
     .arguments('<version>')
     .action(v => { ver = v })
     .parse(process.argv)
 
   if (ver) {
-    let fileStr = fs.readFileSync('./bower.json', 'utf-8')
-    const verPtn = /(.*"version"\s*:\s*")[^"]*(".*)/
-
-    if (verPtn.test(fileStr)) {
-      fileStr = fileStr.replace(verPtn, '$1' + ver + '$2')
-      fs.writeFileSync('./bower.json', fileStr, 'utf-8')
-    }
+    program.file.forEach(
+      file => updateVersion(file, ver)
+    )
   }
 
   process.exit(0)
